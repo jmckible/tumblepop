e3fdfd9e389068aa863ace62c1c6d963cdd7d199
Jordan McKible <jordan@mckible.com>
2010-10-09 02:42:46 -0700

    Generate fresh Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumblepop::Application.config.secret_token = '96340231537d14f86fdf741ba16bf4ab0a90be44d6a81ceaa2748f75ae69c6cd8cfb461d785f98d2364d94dda59e9c718fca9904e63b3e581a59bb965ed0501b'
