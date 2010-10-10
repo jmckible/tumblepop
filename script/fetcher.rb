9c2c8aacd885ed2393ad70ee2d02763879bcefdc
Jordan McKible <jordan@mckible.com>
2010-10-09 23:28:22 -0700

    Fetcher daemon.

--- script/fetcher.rb
+++ script/fetcher.rb
@@ -4,4 +4,8 @@ require File.dirname(__FILE__) + '/../config/environment.rb'
 config = YAML.load_file "#{Rails.root}/config/mail.yml"
 config = config[Rails.env].to_options
 fetcher = Fetcher.create({:receiver=>StoryProcessor}.merge(config))
-fetcher.fetch
+
+loop do
+  fetcher.fetch
+  sleep 60
+end
