5643feb066e2ec7fe30b21181d825741f9d51cac
Jordan McKible <jordan@mckible.com>
2010-10-09 19:23:53 -0700

    Handle multipart messages.

--- app/mailers/story_processor.rb
+++ app/mailers/story_processor.rb
@@ -4,10 +4,16 @@ class StoryProcessor < ActionMailer::Base
     if mail.to.to_s =~ /^stories\+.{8}@tumblepop.com$/
       secret = mail.to.to_s.gsub(/^stories\+/, '').gsub(/@tumblepop.com$/, '')
       ask = Ask.find_by_secret! secret
-      ask.process! mail.from.to_s, mail.subject, mail.body
+      
+      if mail.multipart?
+        ask.process! mail.from.to_s, mail.subject, mail.parts.first.body
+      else
+        ask.process! mail.from.to_s, mail.subject, mail.body
+      end
+      
     else
       raise 'Not a story'
-    end
+    end    
   end
   
 end
